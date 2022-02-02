565c34c1020cac694b611e37de715442a0130989
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-02-02 15:10:50 -0600

    commit tree ea26414caf6f62ad5010bdfd28b306124e3ef1c3

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    if (street > 42) {
+        return (street - 42);
+    }
+    else if (street < 42) {
+        return (42 - street);
+    }
+}
+
+function distanceFromHqInFeet(street) {
+    if (street > 42) {
+        return (street - 42) * 264;
+    }
+    else if (street < 42) {
+        return (42 - street) * 264;
+    }
+}
+
+function distanceTravelledInFeet (start, destination) {
+    if (start > destination) {
+        return (start - destination) * 264;
+    }
+    else if (start < destination) {
+        return (destination - start) * 264;
+    }
+}
+
+function calculatesFarePrice (start, destination) {
+    let n = distanceTravelledInFeet (start, destination)
+    if (n < 400) {
+        return 0;
+    }
+    else if (n > 2500) {
+        return "cannot travel that far";
+    }
+    else if (n > 2000) {
+        return 25;
+    }
+    else if (n >= 400) {
+        return ((n - 400) * .02);
+    }
+}  
